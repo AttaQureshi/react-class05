ca655d7f96990b8243d8c89a2f00c22813f69007
AttaQureshi <attatalhaqureshi@gmail.com>
2020-06-14 23:31:37 +0500

    Initial commit

---
+++ src/Parent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Child from './child.js'
+import Child2 from './Child2.js'
+
+const Parent = ()=> {
+    return (
+        <div>
+        <Child/>
+        <Child2/>
+        </div>
+    )
+
+}
+
+export default Parent;
